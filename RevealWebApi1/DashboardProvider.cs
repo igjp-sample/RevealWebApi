611214ada985c561c1623f13f98ac36bd92674cc
yoshi1220 <19341602+yoshi1220@users.noreply.github.com>
2023-01-31 10:56:54 +0900

    プロジェクト ファイルを追加します。

---
+++ RevealWebApi1/DashboardProvider.cs
@@ -0,0 +1,19 @@
+﻿using Reveal.Sdk;
+
+namespace RevealWebApi1
+{
+    public class DashboardProvider : IRVDashboardProvider
+    {
+        public Task<Dashboard> GetDashboardAsync(IRVUserContext userContext, string dashboardId)
+        {
+            var filePath = Path.Combine(Environment.CurrentDirectory, $"Dashboards/{dashboardId}.rdash");
+            var dashboard = new Dashboard(filePath);
+            return Task.FromResult(dashboard);
+        }
+
+        public Task SaveDashboardAsync(IRVUserContext userContext, string dashboardId, Dashboard dashboard)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RevealWebApi1/Program.cs
@@ -0,0 +1,42 @@
+using Reveal.Sdk;
+using RevealWebApi1;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+      builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()
+    );
+});
+
+builder.Services.AddControllers().AddReveal(builder =>
+{
+    builder.AddDashboardProvider<DashboardProvider>();
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+app.UseAuthorization();
+
+app.MapControllers();
+
+
+
+app.Run();
