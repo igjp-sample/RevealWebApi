67bad96553e712818bec5b0278b42d8420202f03
Motoki Nakae <motoki@cleverdog.me>
2023-04-25 10:19:53 +0900

    Update Program.cs

--- RevealWebApi1/Program.cs
+++ RevealWebApi1/Program.cs
@@ -3,7 +3,7 @@ using RevealWebApi1;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container..
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
